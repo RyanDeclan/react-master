ccfcecdf2787eb2ea99bdf1481a6a671f37371a8
kang-cheol-won <cjfdnjs158@naver.com>
2022-04-14 05:43:45 +0900

    first commit

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,42 +1,53 @@
-import { useSetRecoilState } from "recoil";
-import { IToDo, toDoState } from "../atoms";
+import React, { useEffect } from "react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { Categories, IToDo, toDoState } from "../atoms";
 
 function ToDo({ text, category, id }: IToDo) {
-  const setToDos = useSetRecoilState(toDoState);
+  const rawToDos = useRecoilValue(toDoState);
+  let setToDos = useSetRecoilState(toDoState);
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     const {
       currentTarget: { name },
     } = event;
-    setToDos((oldToDos) => {
-      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
-
-      const newToDo = { text, id, category: name as any };
-      return [
-        ...oldToDos.slice(0, targetIndex),
-        newToDo,
-        ...oldToDos.slice(targetIndex + 1),
-      ];
-    });
+    if (localStorage.getItem("TODOS_KEY") === null) {
+      setToDos((oldToDos) => {
+        const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+        const newToDo = { text, id, category: name as any };
+        return [
+          ...oldToDos.slice(0, targetIndex),
+          newToDo,
+          ...oldToDos.slice(targetIndex + 1),
+        ];
+      });
+    } else {
+      console.log("hi");
+      setToDos(JSON.parse(localStorage.getItem("TODOS_KEY")!));
+    }
   };
+  useEffect(() => {
+    localStorage.setItem("TODOS_KEY", JSON.stringify(rawToDos));
+  }, [rawToDos]);
+
   return (
     <li>
       <span>{text}</span>
-      {category !== "DOING" && (
-        <button name="DOING" onClick={onClick}>
+      {category !== Categories.DOING && (
+        <button name={Categories.DOING} onClick={onClick}>
           Doing
         </button>
       )}
-      {category !== "TO_DO" && (
-        <button name="TO_DO" onClick={onClick}>
+      {category !== Categories.TO_DO && (
+        <button name={Categories.TO_DO} onClick={onClick}>
           To Do
         </button>
       )}
-      {category !== "DONE" && (
-        <button name="DONE" onClick={onClick}>
+      {category !== Categories.DONE && (
+        <button name={Categories.DONE} onClick={onClick}>
           Done
         </button>
       )}
     </li>
   );
 }
+
 export default ToDo;
